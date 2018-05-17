afd6b60b70d8dbae08264a8560d7a3fdcdc7c873
Mikaela Scornaienchi <scornaim@gmail.com>
2018-05-17 15:28:08 -0400

    starting operations

--- dev/scripts/app.js
+++ dev/scripts/app.js
@@ -7,31 +7,63 @@ import ReactDOM from 'react-dom';
 // Make it work and then set up firebase
 // Strech goal is a notepad on the sign, a full bugeting app, strech strech is a signin
 
-// this function will operate the submit window on 
 class App extends React.Component {
   constructor() {
     super();
     this.state = {
-      display: '0', 
+      display: '0',
+      operator: ''
     }
     this.userInput = this.userInput.bind(this);
     this.sendNumber = this.sendNumber.bind(this);
-    // this.onEnter = this.onEnter.bind(this);
-  }
- 
-  // maybe change to componentDidMount() for firebase? 
-  // this will update the view window when the user presses a number
-  userInput(e) {
-    console.log(number);
-    this.setState({
-      display: display + userInput.val()
-    })
-    console.log('button clicked');
+    // this.userEnter = this.userEnter.bind.(this);
   }
   sendNumber(e) {
-    e.stopPropogation();
     e.preventDefault();
+  }
+  // maybe change to componentDidMount() for firebase? 
+  // this will update the view window when the user presses a number
+  userInput(selectedNumber) {
+    console.log(selectedNumber);
+    this.setState({
+      display: this.state.display + selectedNumber
+      // use bottom to get rid of the 0 when they start typing
+      // display: display === '0' ? '0' : this.state.display + selectedNumber
+    })
+    // recall our conversation about boxes and parameters
+    // what is selectedNumber in this case, and how am I shoving a number into it?
+    return selectedNumber;
+  }
 
+  userClear() {
+    console.log('clear');
+    this.setState({
+      display: '0'
+    }) 
+  }
+  userEnter() {
+    console.log('enter');
+    // come here update state and string together the firstNum and the operator and secondNum
+    // then stop to re set state
+    this.setState({
+      // display: this.state.display.toString()
+      display: (`${firstNum}${operator}${lastNum}`)
+
+    })
+  }
+
+  userMath(operator) {
+    console.log(operator);
+    this.setState({
+      // remember what comes before the operator in the first num and then update display to 0 and set state to second num
+      // has to be everything before the operator
+      firstNum: this.state.display,
+      // display: this.state.display 
+      // hold the operator sign
+      operator: operator,
+      secondNum: ''
+
+    })
   }
 
   render() {
@@ -46,25 +78,25 @@ class App extends React.Component {
             <button onClick={() => this.userInput(7)}>7</button>
             <button onClick={() => this.userInput(8)}>8</button>
             <button onClick={() => this.userInput(9)}>9</button>
-            <button>*</button>
+            <button onClick={() => this.userMath('/')}>/</button>
           </div>
           <div>
             <button onClick={() => this.userInput(4)}>4</button>
             <button onClick={() => this.userInput(5)}>5</button>
             <button onClick={() => this.userInput(6)}>6</button>
-            <button>-</button>
+            <button onClick={() => this.userMath('*')}>*</button>
           </div>
           <div>
             <button onClick={() => this.userInput(1)}>1</button>
             <button onClick={() => this.userInput(2)}>2</button>
             <button onClick={() => this.userInput(3)}>3</button>
-            <button>+</button>
+            <button onClick={() => this.userMath('-')}>-</button>
           </div>
           <div>
             <button onClick={() => this.userInput(0)}>0</button>
-            <button>C</button>
-            <button>/</button>
-            <button>=</button>
+            <button onClick={() => this.userClear()}>C</button>
+            <button onClick={() => this.userMath('+')}>+</button>
+            <button onClick={() => this.userEnter('=')}>=</button>
           </div>
         </form>
       </div>
